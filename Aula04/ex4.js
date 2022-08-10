a44fb6b61d2b80be85c61bfd4e5eed90e07cb085
Gabriel-Teodoro <gabrieltodoro1@hotmail.com>
2022-08-09 21:46:30 -0300

    Aula 4

---
+++ Aula04/ex4.js
@@ -0,0 +1,9 @@
+const nome = "Jose"; //Constant 
+const sobrenome = "Da silva"; //Constant 
+let idade = 27; // Variavel
+let peso = 83.5; // Variavel
+let altura = 1.70; // Variavel
+let plano = true; // pode se atribuir Null
+let imc = peso / Math.pow(altura,2); // Variavel
+
+console.log(nome + " " + sobrenome + " Tem " + idade + " anos e seu indice de massa corporal é de " + imc);
