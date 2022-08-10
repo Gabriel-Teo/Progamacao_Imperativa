9455732cc3cce9912645251c2c9f1b6dae1e4be6
Gabriel-Teodoro <gabrieltodoro1@hotmail.com>
2022-08-09 22:10:44 -0300

    correcao null

--- Aula04/ex4.js
+++ Aula04/ex4.js
@@ -3,7 +3,9 @@ const sobrenome = "Da silva"; //Constant
 let idade = 27; // Variavel
 let peso = 83.5; // Variavel
 let altura = 1.70; // Variavel
-let plano = true; // pode se atribuir Null
-let imc = peso / Math.pow(altura,2); // Variavel
+let plano = true; // 
+let imc = null; 
+
+imc = peso / Math.pow(altura,2); // Variavel
 
 console.log(nome + " " + sobrenome + " Tem " + idade + " anos e seu indice de massa corporal é de " + imc);
